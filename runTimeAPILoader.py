03f8e738039e2fb21bd888c262e956d33af17afc
Dylan <drk@dylankoger.com>
2022-11-02 19:56:18 -0500

    modified:   Video.py 	modified:   driver.py 	deleted:    eBOX.py 	deleted:    keys.py 	renamed:    dictionarymaker.py -> offlineLoader.py 	modified:   runTimeAPILoader.py

--- runTimeAPILoader.py
+++ runTimeAPILoader.py
@@ -1,12 +1,10 @@
-import datetime as dt
 from Video import *
 from bs4 import BeautifulSoup, SoupStrainer
 import time
-import pickle
-import keys
 import requests
+import pickle
 #Change String from ISO (YouTube) format to Seconds (Int)
-def fromISOtoSec(isoString):
+def fromISOtoSec(isoString) -> int:
     #Reserved for future use
     # daysPlus = isoString[:isoString.index('T')]
     #Get time numbers
@@ -35,142 +33,93 @@ def fromISOtoSec(isoString):
         totalTime = hours + minutes + seconds
     return totalTime
 #Shift global Keysto a new one that will hopefully work
-def shiftKeys():
+#def shiftKeys() -> None:
+    print(keys.CURRENT_KEY)
     keys.CURRENT_INDEX =  (keys.CURRENT_INDEX + 1) % 12
     keys.CURRENT_KEY =  keys.API_KEYS[keys.CURRENT_INDEX]
     if keys.CURRENT_INDEX == 0:
         exit()
 #Cut out the ID for the video from the Youtube URL
-def getVidIDfromURL(urlStr):
-        print(urlStr)
+def getVidIDfromURL(urlStr) -> str:
+        # print(urlStr)
         return urlStr[urlStr.index('=') + 1:]
 #Cut out channel ID from the channel URL
-def getChannelID(url):
+def getChannelID(url) -> str:
     return url[url.index('channel/') + 8:]
-
-
-
-
-
-def fetchDuration(id):
-    global config_dictionary
-    api_url = 'https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id=' + id + '&key=' + keys.CURRENT_KEY
-    response = requests.get(api_url)
+#extracts date from youtube webpage html
+def extractDate(str) -> str:
+    arr=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    i = -27
+    while i > -100:
+        for month in arr:
+            if month in str[i:]:
+                return str[i:]
+        i -= 1
+#find object and attach date
+def attachDateToObj(id, dict, date) -> None:
+    
+    for obj in dict:
+        if obj.id == id:
+            obj.date = date
+#Creates a video object from the decoded json
+def getVideofromJSON(json) -> Video:
+    youtubeVidBase = "https://www.youtube.com/watch?v="
+    youtubeChannelBase = "https://www.youtube.com/channel/"
     try:
-        return response.json()['items'][0]['contentDetails']['duration']
+        items = json['items']
+        ret = []
+        for video in items:
+            ret.append(Video(youtubeVidBase + video['id'], 
+                youtubeChannelBase + video['snippet']['channelId'],
+                video['id'],
+                fromISOtoSec(video['contentDetails']['duration']),
+                video['snippet']['channelTitle'],
+                video['snippet']['categoryId']))
+        # for i in range(0,len(ret)):
+        #     video.date = dates[i]
+        return ret
     except:
-        if response.json()['error']['code']:
-            # touchBase(response.json(), dt.datetime.now())
-            shiftKeys()
-        print(response.json())
-
-
-
-if __name__ == '__main__':
-    watchHistory = open('watch-history.html', encoding="utf8")
+        print(json)
+        exit()
+# fetches the channel, category name, and duration in one API call
+# returns an array of Video objects
+def fetchAPIinfo(fileName) -> list[Video] :
+    watchHistory = open(fileName, encoding="utf8")
     soup_strainer = SoupStrainer('div', attrs={'class':'content-cell mdl-cell mdl-cell--6-col mdl-typography--body-1'})
     doc = BeautifulSoup(watchHistory, 'html.parser', on_duplicate_attribute='ignore' ,parse_only=soup_strainer)
-    channelLinks = []
-    videoLinks = []
-    videos = []
-    inactiveLinks = 0
-    i = 0
-    beginning = time.perf_counter()
-    totalTimeWatched = 15515418
-    config_dictionary = open('config.dictionary', 'ab')
-    start = False
-    print(str(len(doc('div'))))
+    ret = []
+    ids = []
+    dates = []
     for div in doc('div'):
-        if start:
-            active = len(div('a'))
-            if active > 1:
-                for a in div('a'):
-                    if 'channel' in a['href']:
-                        channelLinks.append(a['href'])
-                    # elif a['href'] in videoLinks:
-                    #     break
-                    else:
-                        videoLinks.append(a['href'])
-                id = getVidIDfromURL(videoLinks[i])
-                duration = fromISOtoSec(fetchDuration(id))
-                clear = True
-                for video in videos:
-                    if id == video.id:
-                        clear = False
-                        break
-                if clear:
-                    totalTimeWatched += duration
-                    video = Video(videoLinks[i], channelLinks[i], id,duration)
-                    videos.append(video)
-                    pickle.dump(video, config_dictionary)
-                    print(totalTimeWatched)
-                i += 1
-            else:
-                inactiveLinks += 1
-        else:
+        active = len(div('a'))
+        if active > 1:
             for a in div('a'):
-                if 'mpjREfvZiDs' in a['href']:
-                    start = True
-    end = time.perf_counter()
-    config_dictionary.close()
-
-#
-def fetchChannelAnndCategoryName(id):
-    global file
-    api_url = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&id=' + id + '&key=AIzaSyAIAyNLdUumVrXStTs8IjZGcVh7xSYD2_8'
-    response = requests.get(api_url)
-    obj = response.json()
-    try:
-        return {'Channel' : obj['items'][0]['snippet']['channelTitle'],
-                'Category': obj['items'][0]['snippet']['categoryId']}
-    except:
-        if response.json()['error']['code']:
-            # touchBase(response.json(), dt.datetime.now())
-            shiftKeys()
-        print(response.json())
-
-def fetchCategoryName(id):
-    global file
-    api_url = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&id=' + id + '&key=AIzaSyDwv7uJfRm7TEiMiAE0XgeQ1YIoYIj2lwA'
-    response = requests.get(api_url)
-    try:
-        return response.json()['items'][0]['snippet']['categoryId']
-    except:
-        # if response.json()['error']['code']:
-        #     touchBase(response.json(), dt.datetime.now())
-        print(response.json())
-        file.close()
-        exit()
-def apiCategoryDriver():
-    w2 = dictionarymaker.loadAll('channel.dictionary')
-    totalLen = len(w2)
-    file = open('video.dictionary', 'ab')
-    i = 0
-    beginning = dt.perf_counter()
-    end = 0
-    t = False
-    elapsed = 0
-    prediction = 0
-    average = 0
-    total = 0
-    offset = 0
-    for video in w2:
-        id = getVidIDfromURL(video.videoURL)
-        category = fetchCategoryName(id)
-        video.category = category
-        pickle.dump(video, file)
-        
-        if i % 150 == 0 and i != 0:
-            total = 0
-            average = 0
-            offset += 1
-        i += 1
-        percentage = (i/totalLen) * 100
-        end = dt.perf_counter()
-        elapsed = (end - beginning)
-        prediction = (elapsed * (totalLen - i)) / 60
-        total += prediction
-        average = total / (i - (150 * offset))
-        beginning = dt.perf_counter()
-        print(str(average), 'minutes\n', str(elapsed))
-        print(str(percentage) + '%')
+                if 'watch' in a['href'] and not (getVidIDfromURL(a['href']) in ids):
+                    ids.append(getVidIDfromURL(a['href']))
+            # dates.append(extractDate(div.getText()))
+    base_api_url = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&part=contentDetails'
+    api_url = base_api_url
+    beginning = time.perf_counter()
+    totalTime = 0
+    num = 1
+    for i in range(0,len(ids)):
+        api_url += '&id=' + ids[i]
+        if (i % 49 == 0 and i != 0) or i == len(ids) - 1:
+            api_url += '&key=AIzaSyAIAyNLdUumVrXStTs8IjZGcVh7xSYD2_8'
+            response = requests.get(api_url)
+            obj = response.json()
+            ret += getVideofromJSON(obj)
+            end = time.perf_counter()
+            elapsed_time = (end - beginning) / 60
+            totalTime += elapsed_time
+            averageTime = ((totalTime) / num)
+            percentageLeft = (1 - (i / len(ids)) )
+            print('Estimated Time Remaining:', ((averageTime * ((percentageLeft * len(ids))/ 50))) * 60, 'seconds')
+            beginning = time.perf_counter()
+            api_url = base_api_url
+            num += 1
+    return ret
+#write dictionary; takes file handle and array of objects
+def saveDictionaryFile(file, array):
+    for item in array:
+        pickle.dump(item, file)
